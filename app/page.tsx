e450ed68d4c0fa07bfae442bd491b18fa61b176a
https://github.com/Sobansaud <159770646+Sobansaud@users.noreply.github.com>
2024-11-08 16:24:29 +0500

    main

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Countdown from "@/components/count-down";
+import React from "react";
+
+export default function Home(){
+    return (
+        <div>
+           < Countdown />
+        </div>
+    )
+}
